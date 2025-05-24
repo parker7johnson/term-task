7c215128781527d67588c16df868bdf199173850
parker7johnson <ptj7@uw.edu>
2025-05-24 16:52:46 -0700

    bare bones of terminal task list

--- main.go
+++ main.go
@@ -5,19 +5,25 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-
 	"golang.org/x/term"
 )
 
 type Task struct {
 	Priority string `json:"priority"`
 	Title    string `json:"title"`
-	DeadLine string `json:"deadLine,omitempty"`
-	Selected bool `json:"selected, omitemtpy"`
+	Selected bool `json:"selected,omitempty"`
+	ToDelete bool `json:"toDelete"`
 }
 
 const (
+	ADD = "add"
+	LIST = "list"
 	FILE_NAME = "tasks.json"
+	UP = 'k' 
+	DOWN = 'j' 
+	DELETE = 'x' 
+	SELECT = 13
+	QUIT = 'q' 
 )
 
 const (
@@ -28,6 +34,159 @@ const (
 )
 
 var selectedIndex int = 0
+var cursor = ""
+var checked = ""
+var tasks []Task = make([]Task, 0)
+
+
+func main() {
+	
+	command := os.Args[1]
+
+	switch command {
+	case ADD:
+		taskTitle := os.Args[2]
+		taskPriority := os.Args[3]
+		add(taskTitle, taskPriority)
+	case LIST:
+		list()
+	}
+
+}
+
+func list() {
+
+	file := readFile()
+
+	err := json.Unmarshal(file, &tasks)
+
+	if err != nil {
+		error(err, UNMARSHAL)
+	}
+
+	
+	displayInteractableList()
+	removeSelectedAndDeletedTasks()
+}
+
+func removeSelectedAndDeletedTasks() {
+	clonedTasks := make([]Task , 0)
+	for _, task := range clonedTasks {
+
+		if task.Selected || task.ToDelete {
+			continue
+		}
+		clonedTasks = append(clonedTasks, task)
+	}
+	tasks = clonedTasks
+	writeTasksToFile()
+}
+
+func displayInteractableList() {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	buf := make([]byte, 3)
+
+
+	redraw(tasks)
+
+	for {
+		os.Stdin.Read(buf)
+		userInput := buf[0]
+		if userInput == QUIT {
+			break
+		}
+		switch userInput {
+		
+		case UP:
+			if selectedIndex > 0 {
+				selectedIndex--
+			}	
+		case DOWN:
+			if selectedIndex < len(tasks) - 1 {
+				selectedIndex++
+			}
+		case SELECT: 
+			tasks[selectedIndex].Selected = !tasks[selectedIndex].Selected
+		case DELETE:
+			tasks[selectedIndex].ToDelete = !tasks[selectedIndex].ToDelete;
+		}
+
+		redraw(tasks)
+	}
+}
+
+func redraw(tasks []Task) {
+	fmt.Print("\033[2J")
+	fmt.Print("\033[H")
+	if len(tasks) == 0 {
+		fmt.Printf("Task List is currently Emtpy")
+	}
+	for i, task := range tasks {
+		
+		if i == selectedIndex {
+			cursor = "> "
+		} else {
+			cursor = ""
+		}
+
+		if task.Selected {
+			checked = "[x]"
+		} else {
+			checked = "[]"
+		}
+		
+		displayTask(task, i)
+	}
+}
+
+func displayTask(task Task, idx int) {
+	if task.ToDelete {
+		fmt.Printf("\u001b[31m%s   %s   %s   %s\u001b[0m", cursor, checked, task.Title, task.Priority)
+	}	else {
+		fmt.Printf("%s   %s   %s   %s", cursor, checked, task.Title, task.Priority)
+	}
+
+	if (len(tasks) - 1 != idx) {
+		fmt.Print("\r\n")
+	}
+}
+
+
+func add(title, priority string) {
+
+	file := readFile()
+
+	newTask := Task{priority, title, false, false}
+
+	err := json.Unmarshal(file, &tasks)
+	if err != nil {
+		error(err, UNMARSHAL)
+	}
+
+	tasks = append(tasks, newTask)
+	
+	fmt.Printf("tasks: %+v", tasks)
+
+	writeTasksToFile()
+}
+
+
+func writeTasksToFile() {
+	data, err := json.Marshal(tasks)	
+	if err != nil {
+		error(err, MARSHAL)
+	}
+
+	err = os.WriteFile(FILE_NAME, data, fs.FileMode(0664))
+	if err != nil {
+		error(err, WRITE)
+	}
+}
 
 func readFile() []byte {
 	file, err := os.ReadFile(FILE_NAME)
@@ -52,121 +211,4 @@ func error(err any, messageType int) {
 		fmt.Printf("Unknown error: %v", err)
 	}
 	os.Exit(1)
-}
-//task add laundry normal tomorrow
-//task list
-//[] 1. Laundry by tomorrow
-func main() {
-	add("laundry", "normal", "tomorrow")
-	add("dishes", "normal", "tomorrow")
-  list()
-
-}
-
-func list() {
-
-	file := readFile()
-	var tasks []Task
-
-	err := json.Unmarshal(file, &tasks)
-
-	if err != nil {
-		error(err, UNMARSHAL)
-	}
-
-	
-	displayInteractableList(tasks)
-}
-
-func displayInteractableList(tasks []Task) {
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		panic(err)
-	}
-	defer term.Restore(int(os.Stdin.Fd()), oldState)
-
-	//this is the same as the code above but manually listens for the ctrl c keys
-	buf := make([]byte, 3)
-
-
-	redraw(tasks)
-	fmt.Print(selectedIndex)
-
-	for {
-		os.Stdin.Read(buf)
-		fmt.Print(buf)	
-
-		if buf[0] == 3 {
-			term.Restore(int(os.Stdin.Fd()), oldState)
-		}
-
-		if buf[0] == 27 && buf[1] == 91 {
-			if buf[2] == 65 {
-				if selectedIndex > 0 {
-					selectedIndex--
-				}
-			} else if buf[2] == 66 {
-				if selectedIndex < len(tasks) {
-					selectedIndex++
-				}
-			}
-		} else if buf[0] == 13 {
-			tasks[selectedIndex].Selected = !tasks[selectedIndex].Selected
-		}
-		redraw(tasks)
-	}
-  
-	
-}
-
-func redraw(tasks []Task) {
-	fmt.Print("\033[2J")
-	fmt.Print("\033[H")
-	checked := "[]"
-	cursor := ""
-	for i, task := range tasks {
-		
-		if i == selectedIndex {
-			cursor = "> "
-		} else {
-			cursor = ""
-		}
-
-		if task.Selected {
-			checked = "[x]"
-		}
-		if (i == len(tasks) - 1) {
-			fmt.Printf("%s %s %s %s %s", cursor, checked, task.Title, task.Priority, task.DeadLine)
-		} else {
-			fmt.Printf("%s %s %s %s %s\r\n", cursor, checked, task.Title, task.Priority, task.DeadLine)
-		}
-	}
-}
-
-
-func add(title, priority, deadline string) {
-
-	file := readFile()
-
-	newTask := Task{priority, title, deadline, false}
-	var tasks []Task 
-
-	err := json.Unmarshal(file, &tasks)
-	if err != nil {
-		error(err, UNMARSHAL)
-	}
-
-	tasks = append(tasks, newTask)
-	
-
-	data, err := json.Marshal(tasks)	
-
-	if err != nil {
-		error(err, MARSHAL)
-	}
-
-	err = os.WriteFile(FILE_NAME, data, fs.FileMode(0664))
-	if err != nil {
-		error(err, WRITE)
-	}
 }
